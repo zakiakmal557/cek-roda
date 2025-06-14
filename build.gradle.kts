af3e73c8b1ef34a20ae6cb789d1dd9dc0f198237
zakiakmal557 <zakiaja888999@gmail.com>
2025-06-14 11:55:06 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.google.firebase.crashlytics) apply false
 }
